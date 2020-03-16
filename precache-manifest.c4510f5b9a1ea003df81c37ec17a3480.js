3aa57604e52dadd363ddd44146df211a3508f413
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2020-03-16 14:18:12 +0000

    Updates

---
+++ precache-manifest.c4510f5b9a1ea003df81c37ec17a3480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ff29114651c0b98e347f859b6cc7dc",
+    "url": "/mandelbrot-react-SLOW/index.html"
+  },
+  {
+    "revision": "cea12830de18d9c57d39",
+    "url": "/mandelbrot-react-SLOW/static/js/2.416b2ee6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mandelbrot-react-SLOW/static/js/2.416b2ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85a76d37442adcbf58c",
+    "url": "/mandelbrot-react-SLOW/static/js/main.d86d0cfb.chunk.js"
+  },
+  {
+    "revision": "69eb6f058fe7cd94c7e8",
+    "url": "/mandelbrot-react-SLOW/static/js/runtime-main.ffb396af.js"
+  }
+]);
